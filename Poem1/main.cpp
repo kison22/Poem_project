942581ec4b3d600b2a266a395992bca3a3a631a8
kison22 <dddd827@ukr.net>
2025-05-26 12:15:51 +0200

    add class Poem

---
+++ Poem1/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <string>
+#include <memory>
+#include <algorithm>
+class Poem {
+protected:
+    std::string title;
+    std::string content;
+
+public:
+    Poem(const std::string& title, const std::string& content)
+        : title(title), content(content) {
+    }
+
+    virtual void display() const = 0;
+    std::string getTitle() const { return title; }
+};
